f0c4d900e6be572cde3bfa13862c0d04cbd29ee0
Alexandru <102241091+alecsim7@users.noreply.github.com>
2025-08-02 16:05:55 +0200

    Serve frontend via Express

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Rotte
+app.get('/', (_, res) => res.sendFile(path.join(__dirname, '../frontend/index.html')));
 app.use('/api', require('./routes/authRoutes'));           // Login, registrazione, logout
 app.use('/api', require('./routes/userRoutes'));           // Profilo utente
 app.use('/api/sedi', require('./routes/sediRoutes'));
