2d875bb7e96000076a544778dea76d18e878b3ec
Alexandru <102241091+alecsim7@users.noreply.github.com>
2025-08-03 22:42:16 +0200

    Fix wrong spazi route path

--- backend/routes/spaziRoutes.js
+++ backend/routes/spaziRoutes.js
@@ -4,7 +4,7 @@ const spaziController = require('../controllers/spaziController');
 const { verificaToken } = require('../middleware/authMiddleware');
 
 // Recupera spazi per sede (accessibile a tutti)
-router.get('/spazi/:sede_id', spaziController.getSpaziPerSede);
+router.get('/:sede_id', spaziController.getSpaziPerSede);
 
 // Aggiungi spazio (protetto, per gestore)
 router.post('/', verificaToken, spaziController.aggiungiSpazio);
