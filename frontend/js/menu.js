53f6fe37523cd282870979fa35f0a8fb426da9f7
Alexandru <102241091+alecsim7@users.noreply.github.com>
2025-08-05 22:34:09 +0200

    feat: reorganize customer menu

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -10,21 +10,22 @@ $(document).ready(function () {
   } else {
     const ruolo = (utente.ruolo || '').toLowerCase();
 
-    // Link comuni a tutti gli utenti autenticati
-    menu.append('<li class="nav-item"><a class="nav-link" href="sedi.html">Sedi</a></li>');
-    menu.append('<li class="nav-item"><a class="nav-link" href="profilo.html">Profilo</a></li>');
-
     // Link specifici per ruolo
     if (ruolo === 'cliente') {
       menu.append('<li class="nav-item"><a class="nav-link" href="dashboard.html">Dashboard</a></li>');
+      menu.append('<li class="nav-item"><a class="nav-link" href="sedi.html">Sedi</a></li>');
       menu.append('<li class="nav-item"><a class="nav-link" href="prenotazione.html">Prenotazioni</a></li>');
       menu.append('<li class="nav-item"><a class="nav-link" href="pagamento.html">Pagamenti</a></li>');
-    }
-    if (ruolo === 'gestore') {
-      menu.append('<li class="nav-item"><a class="nav-link" href="gestore.html">Gestore</a></li>');
-    }
-    if (ruolo === 'admin') {
-      menu.append('<li class="nav-item"><a class="nav-link" href="admin.html">Admin</a></li>');
+      menu.append('<li class="nav-item"><a class="nav-link" href="profilo.html">Profilo</a></li>');
+    } else {
+      menu.append('<li class="nav-item"><a class="nav-link" href="sedi.html">Sedi</a></li>');
+      menu.append('<li class="nav-item"><a class="nav-link" href="profilo.html">Profilo</a></li>');
+      if (ruolo === 'gestore') {
+        menu.append('<li class="nav-item"><a class="nav-link" href="gestore.html">Gestore</a></li>');
+      }
+      if (ruolo === 'admin') {
+        menu.append('<li class="nav-item"><a class="nav-link" href="admin.html">Admin</a></li>');
+      }
     }
 
     $('#nomeUtente').text(utente.nome || '');
