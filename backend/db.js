f1d5d6928381a7e352469620d505cf11beb320de
Alexandru <102241091+alecsim7@users.noreply.github.com>
2025-08-17 18:30:19 +0200

    aa

--- backend/db.js
+++ backend/db.js
@@ -20,6 +20,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT) || 5432,
+  ssl: { rejectUnauthorized: false }
 });
 
 // Verifica immediatamente la connessione al database
